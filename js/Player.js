452aac4362163e638e39f5a957217a312e7457fb
timurridjanovic <timur.ridjanovic@gmail.com>
2013-10-14 17:15:56 -0400

    added collision detection

--- js/Player.js
+++ js/Player.js
@@ -14,6 +14,8 @@ Player = Class.extend({
     
     coordY: 100,
     
+    proposedPosition: {},
+    
     init: function() {
         for (var i = 0; i < gGameEngine.images.length; i++) {
             if (gGameEngine.images[i].alt == "mainPlayer") {
@@ -25,84 +27,68 @@ Player = Class.extend({
     },
     
     drawPlayer: function() {
+        this.proposedPosition = {'x': this.coordX, 'y': this.coordY};
+        var movingFlag = false;
+            
         if (gInputEngine.actions['up'] == true) {
             this.direction = 'up';
+            this.proposedPosition.y -= 1;
+            movingFlag = true;
             
-            //check next tile
-            this.nextTile = [Math.round(this.coordX/32), Math.round(this.coordY/32)];
-            //collision detection
-            if (gGameEngine.collision[this.nextTile] != true) {
-                this.coordY--;
-            }
-            
-            //animation when moving
-            this.movementTime++;
-        
-            if (this.movementTime >= 6) {
-                this.currentFrame = (this.currentFrame%4) + 1;
-                this.movementTime = 0;
-            }
+             //check next tile
+            this.nextTile = [Math.round(this.proposedPosition.x/32), Math.round
+                (this.proposedPosition.y/32)];           
         }
         
         else if (gInputEngine.actions['down'] == true) {
-            this.direction = 'down'
+            this.direction = 'down';
+            this.proposedPosition.y += 1;
+            movingFlag = true;
             
-            //check next tile
-            this.nextTile = [Math.round(this.coordX/32), Math.round(this.coordY/32)];
-            
-            if (gGameEngine.collision[this.nextTile] != true) {
-                this.coordY++;
-            }
-
-            //console.log(gInputEngine.actions);
-            this.movementTime++;
-        
-            if (this.movementTime >= 6) {
-                this.currentFrame = (this.currentFrame%4) + 1;
-                this.movementTime = 0;
-            }            
-        }
-        
+             //check next tile
+            this.nextTile = [Math.round(this.proposedPosition.x/32), Math.ceil
+                (this.proposedPosition.y/32)];                
+        }        
+ 
         else if (gInputEngine.actions['left'] == true) {
-            this.direction = 'left';
+            this.direction = 'left'; 
+            this.proposedPosition.x -= 1;
+            movingFlag = true;
             
-            //check next tile
-            this.nextTile = [Math.round(this.coordX/32), Math.round(this.coordY/32)];
-            
-            //collision detection
-            if (gGameEngine.collision[this.nextTile] != true) {
-                this.coordX--;
-            }
-
-            
-            this.movementTime++;
-        
-            if (this.movementTime >= 6) {
-                this.currentFrame = (this.currentFrame%4) + 1;
-                this.movementTime = 0;
-            }             
+             //check next tile
+            this.nextTile = [Math.floor(this.proposedPosition.x/32), Math.ceil
+                (this.proposedPosition.y/32)];                
         }
-        
         
         else if (gInputEngine.actions['right'] == true) {
-            this.direction = 'right';
+            this.direction = 'right';     
+            this.proposedPosition.x += 1;
+            movingFlag = true;     
             
-            //check next tile
-            this.nextTile = [Math.round(this.coordX/32), Math.round(this.coordY/32)];
+             //check next tile
+            this.nextTile = [Math.ceil(this.proposedPosition.x/32), Math.ceil
+                (this.proposedPosition.y/32)];                   
+        }       
+                      
+    
+        //collision detection
+        if (gGameEngine.collision[this.nextTile] != true) {
+            this.coordY = this.proposedPosition.y;
+            this.coordX = this.proposedPosition.x;
+        }
             
-            //collision detection
-            if (gGameEngine.collision[this.nextTile] != true) {
-                this.coordX++;
-            }         
-            
+        //animation when moving
+        if (movingFlag == true) {
             this.movementTime++;
+            console.log(Math.round(this.coordX/32), Math.round(this.coordY/32));
         
             if (this.movementTime >= 6) {
                 this.currentFrame = (this.currentFrame%4) + 1;
                 this.movementTime = 0;
-            }             
-        }     
-        
+            }        
+        }
+
+       
         gGameEngine.ctx.drawImage(gGameEngine.mainPlayerImg, this.frameX[this.currentFrame], this.frameY[this.direction], 32, 32, this.coordX, this.coordY, 32, 32);
        
     }
